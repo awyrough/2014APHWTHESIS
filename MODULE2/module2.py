10dcae56901f5604cb974d6b08f23fa37a0fec3e
Alexander P. Hill Wyrough <alexander.hill.wyrough@gmail.com>
2014-04-05 20:37:15 -0400

    Update module2.py

--- MODULE2/module2.py
+++ MODULE2/module2.py
@@ -15,20 +15,16 @@ Notes: The structure is inspired by Mufti's Module 2, and get_work_county() help
 updated version of his. 
 
 ''' 
-
 from datetime import datetime
 import csv
 import countyAdjacencyReader
 import industryReader
 import workPlaceHelper
-
 'MAIN PATH ON MY COMPUTER TOWARDS MODULE1 OUTPUT'
 O_PATH = "C:\\Users\\Hill\\Desktop\\Thesis\\Data\\Output\\Module1\\Second Run Complete\\"
 M_PATH = "C:\\Users\\Hill\\Desktop\\Thesis\\Data\\"
-
 'Global Variable for Journey to Work Complete Census Data'
 j2w = []
-   
 'RETURN THE WORK COUNTY GIVEN RESIDENT, GENDER, AGE, HOUSEHOLD TYPE, and TRAVELER TYPE.'
 def get_work_county(homefips, hht, tt):
     global j2wDist
@@ -44,14 +40,12 @@ def get_work_county(homefips, hht, tt):
             return -2
         else:
             return val[1:]
-        
 'READ IN ASSOCIATED STATE ABBREVIATIONS WITH STATE FIPS CODES'
 def read_states():
     stateFileLocation = M_PATH + '\\'
     fname = stateFileLocation + 'ListofStates.csv'
     lines = open(fname).read().splitlines()
     return lines
-
 'WRITE MODULE 2 OUTPUT HEADERS'
 def writeHeaders(pW):
     pW.writerow(['Residence State'] + ['County Code'] + ['Tract Code'] + ['Block Code'] 
@@ -61,8 +55,7 @@ def writeHeaders(pW):
                   + ['Employer'] + ['Work Address'] + ['Work City'] + ['Work State'] 
                   + ['Work Zip'] + ['Work County Name'] + ['NAISC Code'] + ['NAISC Description'] 
                   + ['Patron:Employee'] + ['Patrons'] + ['Employees'] + ['Work Lat'] + ['Work Lon'] )
-     
-'-----------------------------------------------------------------------------------------------------'
+'---------------------------------------------------------------------------------------------------'
 'EXECUTIVE FUNCTION TO ASSIGN WORKERS TO A WORK COUNTY, WORK INDUSTRY, AND WORK PLACE'
 def executive(state):
     global j2w
@@ -117,7 +110,7 @@ def executive(state):
         gender = int(row[10]); age = int(row[9]); hht = int(row[5]); 
         tt = int(row[11]); income = float(row[13]); lat = float(row[7]); lon = float(row[8])
         workCounty = get_work_county(fips, hht, tt)
-        'ASSIGN WORK INDUSTRY AND WORK PLACE-----------------------------------------------------------'
+        'ASSIGN WORK INDUSTRY AND WORK PLACE-------------------------------------------------'
         if (workCounty != -1) and (workCounty != -2):
             'Check If WorkCounty Has Not Already Been Initialized, if not, Add it'
             if workCounty not in workingCounties:
@@ -144,7 +137,7 @@ def executive(state):
                                  + [employer[2]] + [employer[3]] + [employer[4]] + [employer[5]] 
                                  + [employer[9]] + [employer[10]] + [employer[11]] + [employer[12]] 
                                  + [employer[13]] + [employer[15]] + [employer[16]])
-        'PROGRESS REPORTING---------------------------------------------------------------------------'    
+        'PROGRESS REPORTING-------------------------------------------------------------------------'    
         count+=1
         if count % 10000 == 0:
             print(str(count) + ' residents done')
